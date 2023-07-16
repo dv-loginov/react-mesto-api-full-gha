b81546b7f38732156f463bc21898194ba0ff4dae
dv-loginov <dilog@rambler.ru>
2023-07-16 12:24:11 +0300

    fix: cors errors

--- backend/app.js
+++ backend/app.js
@@ -42,7 +42,13 @@ app.use(cookieParser());
 
 app.use(bodyParser.json());
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({
+  origin: [
+    'https://dilog.nomoredomains.xyz',
+    'http://dilog.nomoredomains.xyz',
+    'http://localhost:3000'],
+  credentials: true,
+}));
 
 app.use(requestLogger);
 
